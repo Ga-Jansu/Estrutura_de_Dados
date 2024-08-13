1e4b8033a082feedd8334206e27ea650bda56e02
Ga-Jansu <gabrieljofig@gmail.com>
2024-08-13 14:26:18 -0300

    Estudo Ex6

---
+++ Aula2_Pratica/Ex6+.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int func1(int n){
+    if(n == 0) return(0);
+    return(n + func1(n-1));
+}
+
+void func2(int n){
+    if(n == 0) printf("fim");
+
+    else{
+        printf("\n%d",n);
+        func2(n-1);
+        printf("%d\n",n);
+    }
+}
+
+int func3(int n){
+    if (n == 0) return(1);
+    if (n == 1) return(1);
+    else return(func3(n-1) + 2 * func3(n-2));
+}
+
+int main(){
+    int n = 5,aux;
+
+    aux = func1(n);
+    printf("\n\n\t%d",aux);
+    getchar();
+    getchar();
+    system("clear");
+
+    func2(n);
+    getchar();
+    getchar();
+    system("clear");
+
+    aux = func3(n);
+    printf("\n\n\t%d",aux);
+    getchar();
+    getchar();
+    system("clear");
+
+    return 0;
+}
